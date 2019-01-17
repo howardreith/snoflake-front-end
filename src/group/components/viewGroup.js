4928d1b8446159a29305c68db8546d8d20bff610
howardreith <howard.reith@gmail.com>
2019-01-17 14:07:07 -0500

    View group now displays proper info
    -View group now displays group name, description, and members list

--- src/group/components/viewGroup.js
+++ src/group/components/viewGroup.js
@@ -9,6 +9,10 @@ import apiUrl from '../../apiConfig'
 class ViewGroup extends Component {
   constructor (props) {
     super(props)
+    this.state = {
+      group: {},
+      isLoaded: false
+    }
   }
 
   componentDidMount() {
@@ -19,17 +23,34 @@ class ViewGroup extends Component {
         console.log('res is ' + JSON.stringify(res))
         return res
       })
+      .then(
+        (res) => {
+          this.setState({
+            group: res.group,
+            isLoaded: true
+          })
+        })
       .then(() => flash(messages.getGroupSuccess, 'flash-success'))
       .catch(() => flash(messages.getGroupFailure, 'flash-error'))
   }
 
   render () {
     const user = this.props.user
-    console.log('props is ', this.props)
+    console.log('this.state.group is ', this.state.group)
+    const group = this.state.group
+    const groupMembers = this.state.group.users
+    console.log('members are ', groupMembers)
+    // const groupMembers = this.state.group.users.map((user) => <li key={user.id}>{user.email}</li>)
 
     return (
+
       <div>
-        Just making sure something renders
+        <h1>{group.name}</h1>
+        <h3>{group.description}</h3>
+        <h4>Members:</h4>
+        <ul>
+          {groupMembers && groupMembers.map((member) => <li key={member.id}>{member.email}</li>)}
+        </ul>
       </div>
     )
   }
